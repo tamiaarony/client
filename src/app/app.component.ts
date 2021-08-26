aa0aff8ee1625424d27b95c3d6afdd8a47d56056
sarat <sara0504157494@gmail.com>
2021-08-26 14:19:16 +0300

    capture audio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
 import { ElementRef, ViewChild } from '@angular/core';
+import { loadvideoservice } from './modules/load-video/load-video-service';
 
 @Component({
   selector: 'app-root',
@@ -12,33 +13,38 @@ export class AppComponent {
   constructor(){
    
     this.nav = navigator;
-    this.displayMediaOptions = {
-      video: {
-        cursor: "always"
-      },
-      audio: false
-    };
   }
   @ViewChild('video', {static: true}) video!: ElementRef<HTMLVideoElement>;
-  videoElem:any;
-  nav: any;
-  displayMediaOptions:any;
-  logElem:string = "";
+  @ViewChild('audio', {static: true}) audio!: ElementRef<HTMLAudioElement>;
 
+  videoElem:any;
+  audioElem:any;
+  nav: any;
+  audioCtx:any;
+  streamNode:any;
+  source:any;
 dumpOptionsInfo() {
   const videoTrack = this.videoElem.srcObject.getVideoTracks()[0];
-
   console.info("Track settings:");
   console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
   console.info("Track constraints:");
   console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
 }
+
  async startCapture()
 {
   this.videoElem=this.video.nativeElement;
-  console.log(this.videoElem)
-  try {
-    this.videoElem.srcObject =await this.nav.mediaDevices.getDisplayMedia(this.displayMediaOptions);
+  this.audioElem=this.audio.nativeElement;
+  try { 
+    const stream=await this.nav.mediaDevices.getDisplayMedia({audio: true, video: true}); 
+    this.videoElem.srcObject=stream;
+    this.audioCtx = new AudioContext();
+    this.source = this.audioCtx.createMediaStreamSource(stream)
+    this.streamNode = this.audioCtx.createMediaStreamDestination();
+    this.source.connect(this.streamNode);
+    this.audioElem.controls = true;
+    document.body.appendChild(this.audioElem);
+    this.audioElem.srcObject = this.streamNode.stream;
     this.dumpOptionsInfo();
   } catch(err) {
     console.error("Error: " + err);
